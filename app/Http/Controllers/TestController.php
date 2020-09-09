bf9380ceeb84472b1ee58da6a1e2705adbba4b63
DESKTOP-6GUFCUQ\Jelena <jelence1994@hotmail.rs>
2020-09-09 15:15:29 +0200

    git commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,34 +7,8 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function index() {
-        $data = ["type" => "hawaian", "base" => "cheese crust"];
+        $data = ["type" => "jeja", "base" => "cheese crust"];
         return view('test', $data);
     }
-
-    public function test() {
-        var_dump($this->greaterThan(4, 3));
-    }
-
-    public function isGreaterThasn(int $a, int $b): bool
-    {
-        if ($a > $b) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function isGreaterThan123(int $a, int $b): bool
-    {
-        return $a > $b ? true : false;
-    }
-
-    
-
-    public function isGreaterThan(int $a, int $b): bool
-    {
-        return $a > $b;
-    }
-
 
 }
