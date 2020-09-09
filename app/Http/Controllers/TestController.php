f6efd36740cf858c72949182ac966a7850691c47
DESKTOP-6GUFCUQ\Jelena <jelence1994@hotmail.rs>
2020-09-09 16:22:25 +0200

    alalla

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,8 +7,7 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function index() {
-        $data = ["type" => "COMMIT", "base" => "cheese crust"];
-        return view('test', $data);
+        $data = ["type" => "neke izmene"];
     }
 
 }
