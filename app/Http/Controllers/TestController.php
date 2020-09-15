e0c73332cb3a451ca2b74df020d9852c0b265e82
DESKTOP-6GUFCUQ\Jelena <jelence1994@hotmail.rs>
2020-09-15 15:09:43 +0200

    neki message

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function index() {
-        $data = ["type" => "neke jos"];
+        $data = ["type" => "lalalal neke jos"];
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welco ds me');
 });
 
 Route::get('/test', 'TestController@index');
